04ef0a97c057f7892b2a1630c38df925c7ed3dc9
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2022-10-21 08:34:52 -0300

    Diversos

--- index.js
+++ index.js
@@ -12,124 +12,120 @@ import prettier from "prettier";
 
 const argumentParser = new argparse.ArgumentParser({});
 argumentParser.add_argument("-i", "--input", {
-  required: true,
-  help: "Input directory"
+    required: true,
+    help: "Input directory"
 });
 argumentParser.add_argument("-t", "--template", {
-  help: "Template file (html)"
+    help: "Template file (html)"
 });
 argumentParser.add_argument("-s", "--selector", {
-  help: "Template file (html)",
-  default: "body"
+    help: "Template file (html)",
+    default: "body"
 });
 argumentParser.add_argument("-p", "--port", {
-  help: "Port",
-  default: 80
+    help: "Port",
+    default: 80
 });
-// argumentParser.add_argument("-s", "--selector", {
-//   help: "Template element selector"
-// });
-// argumentParser.add_argument("-v", "--verbose", {
-//   help: "Verbose mode"
-// });
 
 const options = argumentParser.parse_args();
 
 const markdown = markdownIt({ html: true });
-markdown.use(markdownItBracketedSpans);
 markdown.use(markdownItAttrs, {
-  leftDelimiter: "{",
-  rightDelimiter: "}",
-  allowedAttributes: ["id", "class"],
+    leftDelimiter: "{",
+    rightDelimiter: "}",
+    allowedAttributes: ["id", "class"],
 });
+markdown.use(markdownItBracketedSpans);
 
 const app = express();
 
 // Webhook for repository update
 app.post("/api/refresh", (req, res) => {
-  res.status(200).send("refresh");
-  try {
-    child_process.execSync("git reset --hard HEAD");
-    child_process.execSync("git pull");
-    child_process.execSync("npm install");
-    child_process.execSync("systemctl restart zen.docs");
-  } catch (error) {
-    console.error(error);
-  }
+    res.status(200).send("refresh");
+    try {
+        child_process.execSync("git reset --hard HEAD");
+        child_process.execSync("git pull");
+        child_process.execSync("npm install");
+        child_process.execSync("systemctl restart zen.docs");
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 app.get(/.*/, (req, res) => {
-  let _path = req.path;
+    let _path = req.path;
 
-  let parsed;
-  let file;
+    let parsed;
+    let file;
 
-  // Exact match
-  parsed = path.parse(_path);
-  file = (options.input + parsed.dir + "/" + parsed.base).replaceAll("//", "/");
-  if (fs.existsSync(file) && parsed.ext.toLowerCase() != ".md") {
-    // Directory
-    if (fs.statSync(file).isDirectory()) {
-      // Append a trailing / to make sure links are pointing to the same folder
-      if (!_path.endsWith("/")) {
-        res.redirect(301, _path + "/");
-        return;
-      }
-      // Add default filename /index
-      _path = (parsed.dir + "/" + parsed.base + "/index").replaceAll("//", "/");
-    } else {
-      res.sendFile(path.resolve(file));
-      return;
-    }
-  }
-
-  // No extension (append .html)
-  parsed = path.parse(_path);
-  if (parsed.ext == "") {
-    _path = _path + ".html";
     // Exact match
     parsed = path.parse(_path);
     file = (options.input + parsed.dir + "/" + parsed.base).replaceAll("//", "/");
-    if (fs.existsSync(file)) {
-      res.sendFile(path.resolve(file));
-      return;
+    if (fs.existsSync(file) && parsed.ext.toLowerCase() != ".md") {
+        // Directory
+        if (fs.statSync(file).isDirectory()) {
+            // Append a trailing / to make sure links are pointing to the same folder
+            if (!_path.endsWith("/")) {
+                res.redirect(301, _path + "/");
+                return;
+            }
+            // Add default filename /index
+            _path = (parsed.dir + "/" + parsed.base + "/index").replaceAll("//", "/");
+        } else {
+            res.sendFile(path.resolve(file));
+            return;
+        }
     }
-  }
 
-  // Markdown parser
-  if (parsed.ext.toLowerCase() == ".html" || parsed.ext.toLowerCase() == ".md") {
-    file = (options.input + parsed.dir + "/" + parsed.name + ".md").replaceAll("//", "/");
-    if (fs.existsSync(file)) {
-      // Template
-      let template;
-      if (options.template && fs.existsSync(options.template))
-        template = fs.readFileSync(options.template).toString();
-
-      const contentDom = new jsdom.JSDOM(markdown.render(fs.readFileSync(file).toString()));
-
-      let dom;
-      if (template) {
-        dom = new jsdom.JSDOM(template);
-        const node = dom.window.document.querySelector(options.selector);
-        for (const children of contentDom.window.document.body.children)
-          node.append(children.cloneNode(true));
-      } else
-        dom = contentDom;
-
-      // Title (first h1 in markdown file)
-      if (contentDom.window.document.body.children[0]?.tagName == "H1")
-        dom.window.document.title = contentDom.window.document.body.children[0].textContent;
-
-      res.header("Content-Type", "text/html; charset=utf-8");
-      res.send(prettier.format(dom.serialize(), { parser: "html" }));
-      return;
+    // No extension (append .html)
+    parsed = path.parse(_path);
+    if (parsed.ext == "") {
+        _path = _path + ".html";
+        // Exact match
+        parsed = path.parse(_path);
+        file = (options.input + parsed.dir + "/" + parsed.base).replaceAll("//", "/");
+        if (fs.existsSync(file)) {
+            res.sendFile(path.resolve(file));
+            return;
+        }
     }
-  }
 
-  res.status(404).send("Not Found");
-  return;
+    // Markdown parser
+    if (parsed.ext.toLowerCase() == ".html" || parsed.ext.toLowerCase() == ".md") {
+        file = (options.input + parsed.dir + "/" + parsed.name + ".md").replaceAll("//", "/");
+        if (fs.existsSync(file)) {
+            // Template
+            let template;
+            if (options.template && fs.existsSync(options.template))
+                template = fs.readFileSync(options.template).toString();
+
+            const contentDom = new jsdom.JSDOM(markdown.render(fs.readFileSync(file).toString()));
+
+            // const headers = contentDom.window.document.querySelectorAll("body>h1,body>h2,body>h3,body>h4,body>h5,body>h6");
+
+            let dom;
+            if (template) {
+                dom = new jsdom.JSDOM(template);
+                const node = dom.window.document.querySelector(options.selector);
+                for (const children of contentDom.window.document.body.children)
+                    node.append(children.cloneNode(true));
+            } else
+                dom = contentDom;
+
+            // Title (first h1 in markdown file)
+            if (contentDom.window.document.body.children[0]?.tagName == "H1")
+                dom.window.document.title = contentDom.window.document.body.children[0].textContent;
+
+            res.header("Content-Type", "text/html; charset=utf-8");
+            res.send(prettier.format(dom.serialize(), { parser: "html" }));
+            return;
+        }
+    }
+
+    res.status(404).send("Not Found");
+    return;
 });
 
 app.listen(options.port, () => {
-  console.log(`zen.docs listening on port ${options.port}`)
+    console.log(`zen.docs listening on port ${options.port}`)
 });
